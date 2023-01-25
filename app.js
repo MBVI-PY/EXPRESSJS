989a591e0e1087d9bbe8b6a6f83ccd449d0a9585
MBVI-PY <115723482+MBVI-PY@users.noreply.github.com>
2023-01-25 17:31:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response){
-    response.sendFile(__dirname + "/templates/index.html");
+    response.sendFile(__dirname + "/index.html");
 })
 
 app.listen(3000)
